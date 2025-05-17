283493ec0f2356921101ca2eb48897226c5f0041
daniel-gf05 <danielfgayoso@gmail.com>
2025-05-17 22:35:28 +0200

    modes

--- NetworkAnalyzer.cpp
+++ NetworkAnalyzer.cpp
@@ -62,10 +62,34 @@ char *askSource()
 {
     char errbuf[PCAP_ERRBUF_SIZE];
     listDevices();
-    std::cout << "Introduce la interfaz de red para la captura: ";
+    std::cout << "\nIntroduce la interfaz de red para la captura: ";
     std::cin >> source;
     // Aquí debería comprobar si la interfaz existe TO-DO
     return source;
+}
+
+int setMonitorMode()
+{
+    if (!pcap_can_set_rfmon(handle))
+    {
+        std::cout << "El adaptador no puede entrar en modo monitor" << "\n";
+        return -1;
+    }
+    pcap_set_rfmon(handle, 1);
+    std::cout << "Modo monitor activado...";
+    return 0;
+}
+
+int setPromiscMode()
+{
+    if (!pcap_set_promisc(handle, 1))
+    {
+        std::cout << "Error al iniciar el modo promiscuo" << "\n";
+        return -1;
+    }
+
+    std::cout << "Modo promiscuo activado" << "\n";
+    return 0;
 }
 
 // Creo un handle para manejar la captura de paquetes
@@ -105,11 +129,41 @@ int activateHandle()
     return 0;
 }
 
+int selectMode()
+{
+    int mode = 0;
+    bool leave = false;
+    while (!leave)
+    {
+        std::cout << "Introduce el modo a usar \n 1- Modo Monitor \n 2- Modo Promiscuo" << "\n";
+        std::cin >> mode;
+        switch (mode)
+        {
+        case 1:
+            setMonitorMode();
+            leave = true;
+            break;
+
+        case 2:
+            setPromiscMode();
+            leave = true;
+            break;
+
+        default:
+            std::cout << "Selección no válida (1 o 2)" << "\n";
+            break;
+        }
+    }
+
+    return 0;
+}
+
 int program()
 {
     if (checkRoot() != 0){return -1;}
     initPcap();
     createPcap();
     activateHandle();
+    selectMode();
     return 0;
 }
